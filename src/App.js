c78c0b1db6378c4f213a2ed51a19e51e7c340b4f
coderapsan <rapsan363@gmail.com>
2023-05-06 18:21:03 +0545

    Simple Interest Calculator using concept of MUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import "./App.css"
 
-function App() {
+function App(){
+  // const si=0;
+  // function calculateSI(){
+  //     const p= document.getElementById("#p")
+  //     const t= document.getElementById("#t")
+  //     const r= document.getElementById("#r")
+  //     const si= (p*t*r)/100
+  //     return (<h1> {si} </h1>)
+  const [p, setP]=React.useState(0)
+  const [t, setT]=React.useState(0)
+  const [r, setR]=React.useState(0)
+  const [si,setSi]= React.useState(0)
+  const siCalculator=()=>{
+    setSi((p*t*r)/100)
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <nav className='navbar'>
+        <h1> Simple Interest Calculator </h1>
+
+      </nav>
+
+        <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField onChange={(event)=>setP(event.target.value)} label="Principle" variant="outlined" />
+      <TextField onChange={(event)=>setT(event.target.value)} label="Time" variant="outlined" />
+      <TextField onChange={(event)=>setR(event.target.value)} label="Rate" variant="outlined" />
+     
+    </Box>
+    <button className='buttonStyle' onClick={siCalculator} > Calculate </button>
+
+    <h1> Result : {si}  </h1>
+     
     </div>
-  );
-}
+  )
+    }
+
+
+
+ 
+
 
 export default App;
